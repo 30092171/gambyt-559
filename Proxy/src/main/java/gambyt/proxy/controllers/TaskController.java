4650f99e7aa48755a47c63264b936fc405a6b556
Greg Vance <71193788+ga-vance@users.noreply.github.com>
2023-02-09 11:40:49 -0700

    Greg proxy (#2)
    * Update to SpringBoot Application
    
    * Add GetMapping for example
    
    * Add initial routes for api endpoints
    
    * Endpoint for getting a users tasks
    
    * Add user controller with single route for login

--- Proxy/src/main/java/gambyt/proxy/controllers/TaskController.java
+++ Proxy/src/main/java/gambyt/proxy/controllers/TaskController.java
@@ -1,14 +1,34 @@
 package gambyt.proxy.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
 public class TaskController {
+
     @GetMapping("")
-    public String sayHello() {
+    public String getAllTasks() {
+//        Endpoint to return all tickets
         return "Hello World!!";
+    }
+
+    @PostMapping("")
+    public void addNewTask() {
+        // Endpoint to add new task
+    }
+
+    @PutMapping("/{id}")
+    public void updateTask(@PathVariable("id") long id) {
+//    Endpoint to update a given ticket by its id
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable("id") long id) {
+//        Endpoint to delete a task by id
+    }
+
+    @GetMapping("/user/{id}")
+    public void getUserTasks(@PathVariable("id") long id) {
+//        Endpoint to get all of a specific users tasks
     }
 }
