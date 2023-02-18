c649d6427aef01de211f7e78c8bd3d245a6ba1cc
Austin Shawaga <60626400+Retr0-XX@users.noreply.github.com>
2023-02-17 22:18:11 -0700

    Added json-simple
    Added json-simple dependency to pom.
    
    Played around in Main to see if it worked.

--- Backend/src/main/java/Main.java
+++ Backend/src/main/java/Main.java
@@ -1,2 +1,9 @@
+import org.json.simple.JSONObject;  
+
 public class Main {
+	
+	public static void main(String[] args) {
+		JSONObject obj=new JSONObject();
+		System.out.println("Hello World");
+	}
 }
