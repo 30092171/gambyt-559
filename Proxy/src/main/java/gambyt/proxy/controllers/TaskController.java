71d0fe3753cb7131592a69d50ff925009de9127e
nick.zhao <nick.zhao@ucalgary.ca>
2023-02-26 03:02:24 -0700

    finish implementing and testing some TC
    - testing for current methods done, working
    - post, put and delete return OK
    - todo: inbox stuff

--- Proxy/src/main/java/gambyt/proxy/controllers/TaskController.java
+++ Proxy/src/main/java/gambyt/proxy/controllers/TaskController.java
@@ -1,5 +1,7 @@
 package gambyt.proxy.controllers;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,8 +22,8 @@ public class TaskController {
 		try {
 			String url = "rmi://localhost/";
 			server = (RemoteFrontend) Naming.lookup(url + "FrontendImpl");
-			System.out.println("inited stuff");
-			System.out.println(server.getPathToData());
+			System.out.println("RMI Lookup Success");
+			System.out.println("Remote FS location: " + server.getPathToData());
 		} catch (Exception e) {
 			System.out.println("Exception occurred while running client: " + e.toString());
 			e.printStackTrace();
@@ -29,13 +31,22 @@ public class TaskController {
 	}
 
 	@GetMapping("")
-	public String getAllTasks() throws RemoteException {
+	public JSONObject getAllTasks() throws RemoteException {
 //        Endpoint to return all tickets
 		HashMap<String, Ticket> tickets = server.getAllTickets();
-		return tickets.toString();
+		JSONObject wrapper = new JSONObject();
+		JSONArray array = new JSONArray();
+		wrapper.put("tickets", array);
+		tickets.forEach((tID, tic) -> {
+			JSONObject ticpair = new JSONObject();
+			ticpair.put("id", tID);
+			ticpair.put("ticket", tic);
+			array.add(ticpair);
+		});
+		return wrapper;
 	}
 
-	@PostMapping(path = "",
+	@PostMapping(path = "/new",
 			consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> addNewTask(@RequestBody Ticket nt) throws RemoteException {
 		// Endpoint to add new task
@@ -46,27 +57,34 @@ public class TaskController {
 	}
 
 	@PutMapping("/{id}")
-	public ResponseEntity<String> updateTask(@PathVariable("id") long id, @RequestBody Ticket ut) throws RemoteException {
+	public ResponseEntity<String> updateTask(@PathVariable("id") String tID, @RequestBody Ticket ut) throws RemoteException {
 //    Endpoint to update a given ticket by its id
 		ut.PrintTicketInfo();
-		String tID = Long.toString(id);
 		server.updateTicket(tID, ut);
 		return new ResponseEntity<>("OK", HttpStatus.OK);
 	}
 
 	@DeleteMapping("/{id}")
-	public ResponseEntity<String> deleteTask(@PathVariable("id") long id) throws RemoteException {
+	public ResponseEntity<String> deleteTask(@PathVariable("id") String tID) throws RemoteException {
 //        Endpoint to delete a task by id
-		String tID = Long.toString(id);
 		server.deleteTicket(tID);
 		return new ResponseEntity<>("OK", HttpStatus.OK);
 	}
 
 	@GetMapping("/user/{id}")
-	public void getUserTasks(@PathVariable("id") long id) throws RemoteException {
+	public JSONObject getUserTasks(@PathVariable("id") long id) throws RemoteException {
 //        Endpoint to get all of a specific users tasks
 		String uID = Long.toString(id);
 		HashMap<String, Ticket> tickets = server.getTicketByUser(uID);
-		
+		JSONObject wrapper = new JSONObject();
+		JSONArray array = new JSONArray();
+		wrapper.put("tickets", array);
+		tickets.forEach((tID, tic) -> {
+			JSONObject ticpair = new JSONObject();
+			ticpair.put("id", tID);
+			ticpair.put("ticket", tic);
+			array.add(ticpair);
+		});
+		return wrapper;
 	}
 }
