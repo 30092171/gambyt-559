4f560ca129209bec21056a29a6e8d5252ba5c17f
Greg Vance <gregory.vance@ucalgary.ca>
2023-02-26 19:07:33 -0700

    Add @CrossOrigin to handle CORS problem

--- Proxy/src/main/java/gambyt/proxy/controllers/UserController.java
+++ Proxy/src/main/java/gambyt/proxy/controllers/UserController.java
@@ -1,5 +1,6 @@
 package gambyt.proxy.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
+    @CrossOrigin(origins = "*")
     @GetMapping("")
     public void checkLogin() {
 //        Endpoint to login a user
