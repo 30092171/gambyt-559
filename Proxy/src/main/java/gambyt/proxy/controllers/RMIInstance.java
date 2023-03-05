5d0d2e7c02b2f6ed9a2b0160c423a9431ab11792
nick.zhao <nick.zhao@ucalgary.ca>
2023-03-05 04:18:17 -0700

    add some debug info for the ip list

--- Proxy/src/main/java/gambyt/proxy/controllers/RMIInstance.java
+++ Proxy/src/main/java/gambyt/proxy/controllers/RMIInstance.java
@@ -6,18 +6,18 @@ import gambyt.backend.RemoteFrontend;
 
 public class RMIInstance {
 	private static RemoteFrontend[] INSTANCES;
+	private static String[] IPS;
 	private static int INDEX = 0;
 
 	public static void initRMI(String[] ips) {
 		System.out.println("Using the following IP addresses for backend:");
 		if (ips.length == 0) { // use localhost
-			System.out.println("\tlocalhost");
 			INSTANCES = new RemoteFrontend[1];
+			IPS = new String[] { "localhost" };
 			String url = "rmi://localhost/";
 			try {
 				INSTANCES[0] = (RemoteFrontend) Naming.lookup(url + "FrontendImpl");
-				System.out.println("RMI Lookup Success");
-				System.out.println("Remote FS location: " + INSTANCES[0].getPathToData());
+				System.out.println("\tlocalhost (success): " + INSTANCES[0].getPathToData());
 			} catch (Exception e) {
 				System.out.println("IP: localhost");
 				System.out.println("Exception occurred initiating RMI: " + e.toString());
@@ -25,13 +25,12 @@ public class RMIInstance {
 			}
 		} else {
 			INSTANCES = new RemoteFrontend[ips.length];
+			IPS = ips;
 			for (int i = 0; i < ips.length; i++) {
-				System.out.println('\t' + ips[i]);
 				String url = "rmi://" + ips[i] + '/';
 				try {
 					INSTANCES[i] = (RemoteFrontend) Naming.lookup(url + "FrontendImpl");
-					System.out.println("RMI Lookup Success");
-					System.out.println("Remote FS location: " + INSTANCES[i].getPathToData());
+					System.out.println('\t' + ips[i] + " (success): " + INSTANCES[i].getPathToData());
 				} catch (Exception e) {
 					System.out.println("IP: " + ips[i]);
 					System.out.println("Exception occurred initiating RMI: " + e.toString());
@@ -44,6 +43,7 @@ public class RMIInstance {
 	public static RemoteFrontend getInstance() {
 		if (INDEX >= INSTANCES.length)
 			INDEX = 0;
+		System.out.println(INDEX + ": " + IPS[INDEX]);
 		return INSTANCES[INDEX++];
 	}
 }
