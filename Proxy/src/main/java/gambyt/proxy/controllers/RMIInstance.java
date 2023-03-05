9c5ce94e18b3f91e84feeca34bef191290f1102b
nick.zhao <nick.zhao@ucalgary.ca>
2023-03-05 04:05:30 -0700

    implement RR style access to backend
    - untested as of now
    - remove singular reference to server in TaskController
    - change RMIInstance class to give a different reference each call in RR
    fashion
    - Add initializer in Main
    - initialize using server ips as the arguments

--- Proxy/src/main/java/gambyt/proxy/controllers/RMIInstance.java
+++ Proxy/src/main/java/gambyt/proxy/controllers/RMIInstance.java
@@ -5,19 +5,45 @@ import java.rmi.Naming;
 import gambyt.backend.RemoteFrontend;
 
 public class RMIInstance {
-	private static RemoteFrontend INSTANCE;
-	
-	public static RemoteFrontend getInstance() {
-		if(INSTANCE != null) return INSTANCE;
-		try {
+	private static RemoteFrontend[] INSTANCES;
+	private static int INDEX = 0;
+
+	public static void initRMI(String[] ips) {
+		System.out.println("Using the following IP addresses for backend:");
+		if (ips.length == 0) { // use localhost
+			System.out.println("\tlocalhost");
+			INSTANCES = new RemoteFrontend[1];
 			String url = "rmi://localhost/";
-			INSTANCE = (RemoteFrontend) Naming.lookup(url + "FrontendImpl");
-			System.out.println("RMI Lookup Success");
-			System.out.println("Remote FS location: " + INSTANCE.getPathToData());
-		} catch (Exception e) {
-			System.out.println("Exception occurred while running client: " + e.toString());
-			e.printStackTrace();
+			try {
+				INSTANCES[0] = (RemoteFrontend) Naming.lookup(url + "FrontendImpl");
+				System.out.println("RMI Lookup Success");
+				System.out.println("Remote FS location: " + INSTANCES[0].getPathToData());
+			} catch (Exception e) {
+				System.out.println("IP: localhost");
+				System.out.println("Exception occurred initiating RMI: " + e.toString());
+				e.printStackTrace();
+			}
+		} else {
+			INSTANCES = new RemoteFrontend[ips.length];
+			for (int i = 0; i < ips.length; i++) {
+				System.out.println('\t' + ips[i]);
+				String url = "rmi://" + ips[i] + '/';
+				try {
+					INSTANCES[i] = (RemoteFrontend) Naming.lookup(url + "FrontendImpl");
+					System.out.println("RMI Lookup Success");
+					System.out.println("Remote FS location: " + INSTANCES[i].getPathToData());
+				} catch (Exception e) {
+					System.out.println("IP: " + ips[i]);
+					System.out.println("Exception occurred initiating RMI: " + e.toString());
+					e.printStackTrace();
+				}
+			}
 		}
-		return INSTANCE;
+	}
+
+	public static RemoteFrontend getInstance() {
+		if (INDEX >= INSTANCES.length)
+			INDEX = 0;
+		return INSTANCES[INDEX++];
 	}
 }
